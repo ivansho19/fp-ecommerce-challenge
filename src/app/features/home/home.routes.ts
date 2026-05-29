3af5a2b7c0a929bc5a1e51c695ef61b2203592e3
Ivan Manrique <imanrique@seek.pe>
2026-05-28 23:16:55 -0400

    change title home routes

--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const HOME_ROUTES: Routes = [
   {
     path: '',
-    title: 'Home | Product Listing',
+    title: 'Challenge | Frontend FP Ecommerce',
     loadComponent: () =>
       import('./pages/home-page.component').then((m) => m.HomePageComponent)
   }
